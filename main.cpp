d89008a3e2d7195fac331536060a78951ee5d3f9
Thomas Goettlicher <ergomo@goettlicher.de>
2012-01-09 13:51:38 +0100

    specify device

--- main.cpp
+++ main.cpp
@@ -7,40 +7,52 @@
 
 int main(int argc, char *argv[])
 {
-    bool list    = false;
-    bool del     = false;
-    bool settime = false;
-    bool show     = false;
-    bool id     = false;
-    int  nr   = 0;
+    bool list      = false;
+    bool del       = false;
+    bool settime   = false;
+    bool show      = false;
+    bool id        = false;
+    int  nr        = 0;
+    QString device = "/dev/ttyUSB0";
 
-    if ( argc == 2 && strcmp(argv[1], "list" ) == 0 )
+    if ((  argc == 2 || argc == 4 ) && strcmp(argv[1], "list" ) == 0 )
         list=true;
 
-    if ( argc == 2 && strcmp(argv[1], "delete" ) == 0 )
+    if (( argc == 2 || argc == 4 ) &&  strcmp(argv[1], "delete" ) == 0 )
         del=true;
 
-    if ( argc == 2 && strcmp(argv[1], "settime" ) == 0 )
+    if (( argc == 2 || argc == 4 ) && strcmp(argv[1], "settime" ) == 0 )
         settime=true;
 
-    if ( argc == 2 && strcmp(argv[1], "id" ) == 0 )
+    if ((  argc == 2 || argc == 4)  && strcmp(argv[1], "id" ) == 0 )
         id=true;
 
 
-    if ( argc == 3 && strcmp(argv[1], "show" ) == 0 )
+    if (( argc == 3 || argc == 5 ) && strcmp(argv[1], "show" ) == 0 )
     {
         show=true;
         nr=atoi(argv[2]);
     }
 
+    if ( argc == 5 && strcmp(argv[3], "-d" ) == 0 )
+    {
+        device=QString(argv[4]);
+    }
+
+    if ( argc == 4 && strcmp(argv[2], "-d" ) == 0 )
+    {
+        device=QString(argv[3]);
+    }
+
+
     if ( !list && !del && !settime && !show && !id )
     {
-       std::cout << "usage: ergomo [id|list|delete|settime]" << std::endl;
-       std::cout << "       ergomo show nr" << std::endl;
+       std::cout << "usage: ergomo {id|list|delete|settime} [-d /dev/ttydevice]" << std::endl;
+       std::cout << "       ergomo show nr [-d /dev/ttydevice]" << std::endl;
        exit (1); 
     }
 
-    Ergomo ergomo("/dev/ttyUSB0");
+    Ergomo ergomo(device);
 
     if ( ! ergomo.connectionTest() ) 
     {
